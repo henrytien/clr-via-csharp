a25c7aa8d5cb41ab7d8ebdf1416cf912f633b6ff
henrytine <tianhangyu8886@gmail.com>
2020-06-09 20:35:28 +0800

    DelayDemo with async and await with Task.Delay. I love mj.

--- src/code/Ch27-1-ComputeOps.cs
+++ src/code/Ch27-1-ComputeOps.cs
@@ -21,8 +21,8 @@ public static class ComputeOps {
         //TaskDemo.Go();
         //ParallelDemo.Go();
         //ParallelLinq.Go();
-        TimerDemo.Go();
-        //DelayDemo.Go();
+        //TimerDemo.Go();
+        DelayDemo.Go();
         //FalseSharing.Go();
     }
 }
@@ -600,17 +600,17 @@ internal static class TimerDemo {
 internal static class DelayDemo {
    public static void Go() {
       Console.WriteLine("Checking status every 2 seconds");
-      Status();
+      Status("Henry love Mj");
 
       Console.ReadLine();   // Prevent the process from terminating
    }
 
    // This method can take whatever parameters you desire
-   private static async void Status() {
+   private static async void Status(String str) {
       while (true) {
-         Console.WriteLine("Checking status at {0}", DateTime.Now);
+         Console.WriteLine("Mj, I miss you at {0}", DateTime.Now);
          // Put code to check status here...
-
+         Console.WriteLine(str + " {0}", DateTime.Now);
          // At end of loop, delay 2 seconds without blocking a thread
          await Task.Delay(2000); // await allows thread to return
          // After 2 seconds, some thread will continue after await to loop around
